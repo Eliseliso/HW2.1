70ad4c2659e0cd4eb4d332c7a3150e5b41079684
Elis <eliso1190@yandex.ru>
2023-01-23 23:10:49 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трёхзначное число : ");
+int num = int.Parse(Console.ReadLine());
+int result = ((num / 10) % 10);
+Console.WriteLine("Вторая цифра  числа :" + result );
+
